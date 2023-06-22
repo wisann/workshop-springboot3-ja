6637b541809ad61798ed39c314852485ae50d613
Wisan <wisansilva3@gmail.com>
2023-06-22 18:51:46 -0300

    user entity and resource

---
+++ src/main/java/com/wisan/project/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.wisan.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wisan.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1l, "maria", "maria@gmail.com", "94658741", "brasil123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+
+}
