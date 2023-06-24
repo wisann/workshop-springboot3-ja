612adf36ec0517d2c016501c07274ea6c494627d
Wisan <wisansilva3@gmail.com>
2023-06-24 15:41:28 -0300

    Product

---
+++ src/main/java/com/wisan/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.wisan.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wisan.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ src/main/java/com/wisan/project/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.wisan.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wisan.project.entities.Product;
+import com.wisan.project.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+
+	public Product findById(Long id) {
+		return productRepository.findById(id).get();
+	}
+}
