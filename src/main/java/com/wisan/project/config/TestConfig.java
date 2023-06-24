b2e27a9f1082df396ce3b4feec608125b365644f
Wisan <wisansilva3@gmail.com>
2023-06-24 11:30:45 -0300

    Category

--- src/main/java/com/wisan/project/config/TestConfig.java
+++ src/main/java/com/wisan/project/config/TestConfig.java
@@ -1,15 +1,18 @@
 package com.wisan.project.config;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.wisan.project.entities.Category;
 import com.wisan.project.entities.Order;
 import com.wisan.project.entities.User;
 import com.wisan.project.entities.enums.OrderStatus;
+import com.wisan.project.repositories.CategoryRepository;
 import com.wisan.project.repositories.OrderRepository;
 import com.wisan.project.repositories.UserRepository;
 
@@ -21,9 +24,16 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
---
+++ src/main/java/com/wisan/project/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.wisan.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wisan.project.entities.Category;
+import com.wisan.project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		return categoryRepository.findById(id).get();
+	}
+
+}
