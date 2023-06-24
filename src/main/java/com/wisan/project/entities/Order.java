2957febbf65a820460f6788281135206e89796c0
Wisan <wisansilva3@gmail.com>
2023-06-24 11:13:35 -0300

    OrderStatus enum

--- src/main/java/com/wisan/project/entities/Order.java
+++ src/main/java/com/wisan/project/entities/Order.java
@@ -5,9 +5,11 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.wisan.project.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,17 +29,21 @@ public class Order implements Serializable {
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
 
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		this.orderStatus = orderStatus;
 	}
 
 	public Order() {
@@ -66,6 +72,14 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
 	}
 
 	@Override
---
+++ src/main/java/com/wisan/project/entities/enums/OrderStatus.java
@@ -0,0 +1,24 @@
+package com.wisan.project.entities.enums;
+
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	
+}
+
