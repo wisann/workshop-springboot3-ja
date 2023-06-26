502829170c7f097cce6400b94bdc5533ad73ceb2
Wisan <wisansilva3@gmail.com>
2023-06-26 19:19:51 -0300

    User update

--- src/main/java/com/wisan/project/service/UserService.java
+++ src/main/java/com/wisan/project/service/UserService.java
@@ -23,12 +23,25 @@ public class UserService {
 	}
 	
 	public User insert(User obj) {
-		return userRepository.save(obj);
+		return userRepository.saveAndFlush(obj);
 		
 	}
 	
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.saveAndFlush(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 
 }
