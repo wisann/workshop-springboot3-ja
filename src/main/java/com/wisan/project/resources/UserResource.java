277de2709a9376eb9cfad47353dd8d367a2fd558
Wisan <wisansilva3@gmail.com>
2023-06-26 18:31:27 -0300

    User insert

--- src/main/java/com/wisan/project/resources/UserResource.java
+++ src/main/java/com/wisan/project/resources/UserResource.java
@@ -1,18 +1,20 @@
 package com.wisan.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wisan.project.entities.User;
 import com.wisan.project.service.UserService;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,6 +33,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
 	}
 
 }
--- src/main/java/com/wisan/project/service/UserService.java
+++ src/main/java/com/wisan/project/service/UserService.java
@@ -10,16 +10,21 @@ import com.wisan.project.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		return userRepository.findById(id).get();
-		}
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+		
+	}
 
 }
